b2787659b6c3da8d38556eee92a3508c6334eaa6
guzmanalejandra <77361246+guzmanalejandra@users.noreply.github.com>
2023-08-08 19:19:57 -0600

    .

---
+++ client.js
@@ -0,0 +1,9 @@
+const net = require('net');
+const readline = require('readline');
+const {encode} = require('js-base64');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
